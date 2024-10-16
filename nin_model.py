01544b4a5503b3273b9dd16b8e553a3229b04f97
conpast <2269252512@qq.com>
2024-10-16 22:05:23 +0800

    first commit

---
+++ nin_model.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
